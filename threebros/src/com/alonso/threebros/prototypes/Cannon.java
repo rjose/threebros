8cc50995cec2962368a16474f8f669858ff18c5b
Rino Jose <rjose@ejorp.com>
2014-02-08 13:09:52 -0800

    Add source code and assets

---
+++ threebros/src/com/alonso/threebros/prototypes/Cannon.java
@@ -0,0 +1,10 @@
+package com.alonso.threebros.prototypes;
+
+public class Cannon extends GameObject {
+    public float angle;
+    
+    public Cannon(float x, float y, float width, float height) {
+        super(x, y, width, height);
+        angle = 0;
+    }
+}
